fdc4c5aada1ff867110c96b3bcd3ae34c3064e78
Emil Auguste <emilrauguste@gmail.com>
2026-06-18 00:15:51 -0400

    feat: implement JSON publishing and update main server logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,12 +5,15 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/Boopitty/learn-pub-sub-starter/internal/pubsub"
+	"github.com/Boopitty/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	fmt.Println("Starting Peril server...")
 
+	// Create a connection
 	connStr := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
@@ -19,6 +22,21 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connection Successful")
+
+	// make a channel using the connection
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Failed to create channel: %v", err)
+		return
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		fmt.Printf("Failed to publishJSON: %v", err)
+		return
+	}
 
 	// wait for ctrl+c to exit
 	signalChan := make(chan os.Signal, 1)
